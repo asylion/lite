b00fc910e6f8124e76a6caaf618d3c7a050a4a2b
Felix Lu <felixlu.public@gmail.com>
2019-06-24 20:37:11 -0700

    Add option to run files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::env;
+use std::fs;
+
 mod ast;
 mod builtins;
 mod environment;
@@ -14,5 +17,17 @@ use lexer::Lexer;
 use parser::Parser;
 
 fn main() {
-    repl::start();
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        repl::start();
+    } else {
+        let file_name = &args[1];
+        let input = fs::read_to_string(file_name).expect("Failed to read file");
+
+        let mut interpreter = Interpreter::new();
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        interpreter.evaluate_stmt(&parser.parse_program());
+    }
 }
